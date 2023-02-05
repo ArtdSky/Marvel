174eeee758d7d52d5472a141b8105f71ad264c17
Art D <adpm@list.ru>
2023-02-05 20:06:53 +0600

    delete log

--- app/src/main/java/com/example/marvel/MarvelApplication.kt
+++ app/src/main/java/com/example/marvel/MarvelApplication.kt
@@ -1,13 +1,9 @@
 package com.example.marvel
 
 import android.app.Application
-import com.example.marvel.data.local.MarvelDatabase
-import com.example.marvel.data.local.MarvelRepository
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MarvelApplication : Application()
 
-//    private val database by lazy { MarvelDatabase.getDatabase(this) }
-//    val repository by lazy { MarvelRepository(database.marvelDao()) }
 
